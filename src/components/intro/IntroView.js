2b47dd1c0aad5c51f9e15784e4e8e582881797e8
odax <nr10@albion.edu>
2018-06-30 15:57:34 -0400

    worked on IntroView.js/scss & Quote.js -added a quote to Quote.js -added font Quattrocento -added hover effect on continue button -swapped header images -added vh based window height

--- src/components/intro/IntroView.js
+++ src/components/intro/IntroView.js
@@ -10,13 +10,17 @@ import name from '../../assets/intro_desktop_assets/name.png';
 class IntroView extends Component {
   render() {
     return (
-      <div className = 'introView_ parent'>
-        <div className = 'introView_children'>
-          <img src = {grafitti} className = 'intro_grafitti' alt = 'stylish one'/>
-          <img src = {name} className = 'intro_name' alt = 'Full Stack Developer Nahlee Riddell'/>
+      <div className = 'introView_parent'>
+          <img src = {mosaic} className='intro_mosaic' alt = 'artwork background'/>
+
+          <div className = 'nameContainer'>
+            <img src = {name} className = 'intro_name' alt = 'Full Stack Developer Nahlee Riddell'/>
+          </div>
           <Quote/>
-          <img src = {mosaic} className='intro_mosaic' alt = 'stylish two'/>
-        </div>
+          <div className = 'text_container'>
+            <div className = 'text'>Continue</div>
+          </div>
+          <img src = {grafitti} className = 'intro_grafitti' alt = 'brick wall with grafitti'/>
       </div>
     )
   }
--- src/components/intro/Quote.js
+++ src/components/intro/Quote.js
@@ -7,11 +7,13 @@ const quoteList = {
     1: ['The Internet?  Is that thing still around?',   
         'Homer Simpson'],
     2: ["Just remember: you’re not a ‘dummy,’ no matter what those computer books claim.  The real dummies are the people who–though technically expert–couldn’t design hardware and software that’s usable by normal consumers if their lives depended upon it.",
-        'Walter Mossberg']
+        'Walter Mossberg'],
+    3: ["You can avoid reality, but you cannot avoid the consequences of avoiding reality.", 
+        "Ayn Rand"]
 };
 
 const quoteGen = () => {
-    return quoteList[Math.floor(Math.random() * 3)];
+    return quoteList[Math.floor(Math.random() * 4)];
 }
 
 export default class Quote extends Component {
