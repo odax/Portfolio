01575565dcf5b22aa89564556f0ab8c374ca4bde
odax <nr10@albion.edu>
2018-07-08 23:33:19 -0400

    finished main page for desktop view, need to restructure form in MainView.sass

--- src/components/main/ContactForm.js
+++ src/components/main/ContactForm.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import './MainView.css';
 import axios from 'axios';
+import coffee_line from '../../assets/desktop_assets/coffee_line.png';
+import submit from '../../assets/desktop_assets/submit.png';
+
 
 export default class ContactForm extends Component {
     constructor() {
@@ -30,7 +33,7 @@ export default class ContactForm extends Component {
             message: this.state.message,
             id: {id}
         }
-        axios.post('myurlhere', {contactInfo})
+        axios.post('https://naleesportfolio.herokuapp.com/main', {contactInfo})
             .then(res => {
                 console.log(res);
                 console.log(res.data);
@@ -40,33 +43,39 @@ export default class ContactForm extends Component {
     return (
         <div>
           <div className = 'contact_form'>
-            <div className = 'contact_form_header'>
-              <div className = 'contact_form_header_title'>
-                Contact
+            <div className = 'contact_form_center'>
+              <div className = 'contact_form_header'>
+                <div className = 'contact_form_header_title'>
+                  Contact
+                </div>
+                <div className = 'contact_form_header_via'>
+                    through form or email
+                </div>
+                <div className = 'contact_form_header_email'>
+                  nr10@albion.edu
+                </div>
               </div>
-              <div className = 'contact_form_header_via'>
-                via below or email
-              </div>
-              <div className = 'contact_form_header_email'>
-                nr10@albion.edu
-              </div>
-            </div>
-            <div className = 'contact_line'>
-            line
-            </div>
-              <form className = 'form' onSubmit ={this.onSubmit}>
-                <label>
-                    <input type='text' name='name' placeholder= 'Name' onChange={this.onChangeName}/>
-                </label>
-                <label>
-                  <input type='text' name='email' placeholder= 'email' onChange={this.onChangeEmail}/>
 
-                </label>
-                <label>
-                  <input type='text' name='message' placeholder ='message' onChange={this.onChangeMessage}/>
-                </label>
-                <button type='submit'>Submit</button>
-              </form>
+              <img src = {coffee_line} className='coffee_line_img' alt = 'line'/>
+
+                <form className = 'form' onSubmit ={this.onSubmit}>
+                  <label>
+                      <div className= 'form_name'>Name:</div>
+                      <input type='text' name='name' onChange={this.onChangeName} className= 'input_name'/>
+                  </label>
+                  <label>
+                    <div className= 'form_email'>Email:</div>
+                    <input type='text' name='email' onChange={this.onChangeEmail} className= 'input_email'/>
+
+                  </label>
+                  <label>
+                    <div className = 'form_message'>Message:</div>
+                    <textarea type='text' name='message' onChange={this.onChangeMessage} className= 'input_message'/>
+                  </label>
+
+                  <button type='submit'><img src = {submit} className='submitbutton' alt = 'submit'/></button>
+                </form>
+              </div>
           </div>
         </div>
     )
