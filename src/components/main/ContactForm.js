cd4b3878815044c9492b76c618b934790a0c0aae
odax <nr10@albion.edu>
2018-07-12 18:33:04 -0400

    completed responsive sizing for apps and contact, trimmed nametitle image, need to work on responsiveness of nametitle and skills

--- src/components/main/ContactForm.js
+++ src/components/main/ContactForm.js
@@ -1,100 +1,131 @@
-import React, { Component } from 'react'
-import './MainView.css';
-import axios from 'axios';
-import coffee_line from '../../assets/desktop_assets/coffee_line.png';
-import submit from '../../assets/desktop_assets/submit.png';
-
+import React, { Component } from "react";
+import "./MainView.css";
+import axios from "axios";
+import coffee_line from "../../assets/desktop_assets/coffee_line.png";
 
 export default class ContactForm extends Component {
-    constructor() {
-        super();
-        this.state = {
-            name: '',
-            email: '',
-            message: '',
-            messagebox: '',
-            messageboxcolor: ''
-        };
-    }
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      message: "",
+      messagebox: "",
+      messageboxcolor: ""
+    };
+  }
 
-    onChangeName = (event) => {
-        this.setState({name: event.target.value});
+  onChangeName = event => {
+    this.setState({ name: event.target.value });
+  };
+  onChangeEmail = event => {
+    this.setState({ email: event.target.value });
+  };
+  onChangeMessage = event => {
+    this.setState({ message: event.target.value });
+  };
+  submitChecker = event => {
+    event.preventDefault();
+    if (this.state.name && this.state.email && this.state.message !== "") {
+      this.onSubmitSuccess(event);
+    } else {
+      this.onSubmitFailure();
     }
-    onChangeEmail = (event) => {
-        this.setState({email: event.target.value});
-    }
-    onChangeMessage = (event) => {
-        this.setState({message: event.target.value});
-    }
-    submitChecker = (event) => {
-        event.preventDefault();
-        if (this.state.name && this.state.email && this.state.message !== '') {
-            this.onSubmitSuccess(event);
-        } else {
-            this.onSubmitFailure();
-        }
-    }
-    onSubmitFailure = () => {
-        console.log('Please fill out the whole form before you try sending!');
-        this.setState({messagebox: 'Please fill out the all fields and then try submitting again!'});
-        this.setState({messageboxcolor: 'red'});
-    }
-    onSubmitSuccess = (event) => {
-        const id = Math.floor(Math.random() * (10000 - 1 + 1) + 1);
-        const contactInfo = {
-            name: this.state.name,
-            email: this.state.email,
-            message: this.state.message,
-            id
-        }
-        axios.post('https://naleesportfolio.herokuapp.com/main', contactInfo)
-            .then(res => {
-                console.log(res);
-                this.setState({name: '', email: '', message: '', messagebox: 'Message Sent Successfully!', messageboxcolor: 'green'})
-                console.log('message sent!')
-            })
-            .catch(err => {
-                this.setState({messagebox: 'Sorry! There was a problem on the server, message not sent. Try emailing me!'});
-            })
-    }
+  };
+  onSubmitFailure = () => {
+    console.log("Please fill out the whole form before you try sending!");
+    this.setState({
+      messagebox:
+        "Please fill out the all fields and then try submitting again!"
+    });
+    this.setState({ messageboxcolor: "red" });
+  };
+  onSubmitSuccess = event => {
+    const id = Math.floor(Math.random() * (10000 - 1 + 1) + 1);
+    const contactInfo = {
+      name: this.state.name,
+      email: this.state.email,
+      message: this.state.message,
+      id
+    };
+    axios
+      .post("https://naleesportfolio.herokuapp.com/main", contactInfo)
+      .then(res => {
+        console.log(res);
+        this.setState({
+          name: "",
+          email: "",
+          message: "",
+          messagebox: "Message Sent Successfully!",
+          messageboxcolor: "green"
+        });
+        console.log("message sent!");
+      })
+      .catch(err => {
+        this.setState({
+          messagebox:
+            "Sorry! There was a problem on the server, message not sent. Try emailing me!"
+        });
+      });
+  };
   render() {
     return (
-        <div>
-          <div className = 'contact_form'>
-            <div className = 'contact_form_center'>
-              <div className = 'contact_form_header'>
-                <div className = 'contact_form_header_title'>
-                  Contact
-                </div>
-                <div className = 'contact_form_header_email'>
-                  Email: nr10@albion.edu
+      <div>
+        <div className="contact_form">
+          <div className="contact_form_center">
+            <div className="contact_form_header">
+              <div className="contact_form_header_title">Contact</div>
+              <div className="contact_form_header_email">
+                Email: nr10@albion.edu
+              </div>
+            </div>
+
+            <form className="form" onSubmit={this.submitChecker}>
+              <label>
+                <div className="form_name">Name:</div>
+                <input
+                  type="text"
+                  name="name"
+                  onChange={this.onChangeName}
+                  className="input_name"
+                  value={this.state.name}
+                />
+              </label>
+              <label>
+                <div className="form_email">Email:</div>
+                <input
+                  type="text"
+                  name="email"
+                  onChange={this.onChangeEmail}
+                  className="input_email"
+                  value={this.state.email}
+                />
+              </label>
+              <label>
+                <div className="form_message">Message:</div>
+                <textarea
+                  type="text"
+                  name="message"
+                  onChange={this.onChangeMessage}
+                  className="input_message"
+                  value={this.state.message}
+                />
+              </label>
+              <div className="button_message">
+                <button type="submit" className="button">
+                  <span>Submit</span>
+                </button>
+                <div
+                  className="messagebox"
+                  style={{ color: this.state.messageboxcolor }}
+                >
+                  {this.state.messagebox}
                 </div>
               </div>
-
-              <img src = {coffee_line} className='coffee_line_img' alt = 'line'/>
-
-                <form className = 'form' onSubmit ={this.submitChecker}>
-                  <label>
-                      <div className= 'form_name'>Name:</div>
-                      <input type='text' name='name' onChange={this.onChangeName} className= 'input_name' value={this.state.name}/>
-                  </label>
-                  <label>
-                    <div className= 'form_email'>Email:</div>
-                    <input type='text' name='email' onChange={this.onChangeEmail} className= 'input_email' value={this.state.email}/>
-
-                  </label>
-                  <label>
-                    <div className = 'form_message'>Message:</div>
-                    <textarea type='text' name='message' onChange={this.onChangeMessage} className= 'input_message' value={this.state.message}/>
-                  </label>
-                  <div className= 'button_message'>
-                    <button type='submit' class='button'><span>Submit</span></button>
-                    <div className= 'messagebox' style = {{color:this.state.messageboxcolor}}>{this.state.messagebox}</div>
-                  </div>
-                </form>
-              </div>
+            </form>
           </div>
         </div>
-    )
+      </div>
+    );
   }
 }
--- src/components/main/MainView.js
+++ src/components/main/MainView.js
@@ -1,146 +1,155 @@
-import React, { Component } from 'react'
-import './MainView.css';
-import ContactForm from './ContactForm';
-import coffee from '../../assets/desktop_assets/coffee.png';
-import GitHub_Logo from '../../assets/desktop_assets/GitHub_Logo.png';
-import grafitti from '../../assets/desktop_assets/grafitti.png';
-import linkedin from '../../assets/desktop_assets/linked-in-logo-key.png';
-import logsnow from '../../assets/desktop_assets/logsnow.png';
-import name from '../../assets/desktop_assets/namesingle.png';
-import tech from '../../assets/desktop_assets/tech.png';
-import vivahands from '../../assets/desktop_assets/Vivahands.png';
-import abstract from '../../assets/desktop_assets/abstract.png';
-import line from '../../assets/desktop_assets/Line.png';
+import React, { Component } from "react";
+import "./MainView.css";
+import ContactForm from "./ContactForm";
+import coffee from "../../assets/desktop_assets/coffee.png";
+import GitHub_Logo from "../../assets/desktop_assets/GitHub_Logo.png";
+import grafitti from "../../assets/desktop_assets/grafitti.png";
+import linkedin from "../../assets/desktop_assets/linked-in-logo-key.png";
+import logsnow from "../../assets/desktop_assets/logsnow.png";
+import name from "../../assets/desktop_assets/namesingle.png";
+import tech from "../../assets/desktop_assets/tech.png";
+import vivahands from "../../assets/desktop_assets/Vivahands.png";
+import abstract from "../../assets/desktop_assets/abstract.png";
+import line from "../../assets/desktop_assets/Line.png";
 
 export default class MainView extends Component {
   render() {
     return (
-      <div className = 'main_container'>
-        <div className = 'center_container'>
-          <div className = 'header'>
-            <div className = 'header_nav'>
-              <a href='#appsid' className='header_nav_button'>Apps</a>
-              <a href='#skillsid' className='header_nav_button'>Skills</a>
-              <a href='#contactid' className='header_nav_button'>Contact</a>
+      <div className="main_container">
+        <div className="center_container">
+          <div className="header">
+            <div className="header_nav">
+              <a href="#appsid" className="header_nav_button">
+                Apps
+              </a>
+              <a href="#skillsid" className="header_nav_button">
+                Skills
+              </a>
+              <a href="#contactid" className="header_nav_button">
+                Contact
+              </a>
             </div>
-            <div className = 'header_links'>
-            <a target="_blank" href="https://github.com/odax"><img src = {GitHub_Logo} className='github' alt = 'github'/></a>
-            <a target="_blank" href="https://www.linkedin.com/in/nalee-riddell/"><img src = {linkedin} className='linkedin' alt = 'linked in'/></a>
-          </div>
-          </div>
-          <div className = 'title'>
-            <img src = {abstract} className='abstract' alt = 'abstract art'/>
-            <img src = {name} className= 'name' alt = 'Nalee Riddell'/>
-            <img src = {grafitti} className = 'grafitti' alt = 'grafitti art'/>
-          </div>
-          <a className = 'apps' id='appsid'>
-            <div className ='apps_top'>
-              <span className = 'apps_title'>Apps</span>
-              <span className = 'apps_playstore'>availible on the playstore</span>
+            <div className="header_links">
+              <a target="_blank" href="https://github.com/odax">
+                <img src={GitHub_Logo} className="github" alt="github" />
+              </a>
+              <a
+                target="_blank"
+                href="https://www.linkedin.com/in/nalee-riddell/"
+              >
+                <img src={linkedin} className="linkedin" alt="linked in" />
+              </a>
             </div>
-            <div className ='apps_line'>
-              <img src = {line} className='line_img' alt = 'line'/>
+          </div>
+          <div className="title">
+            <img src={abstract} className="abstract" alt="abstract art" />
+            <img src={name} className="name" alt="Nalee Riddell" />
+            <img src={grafitti} className="grafitti" alt="grafitti art" />
+          </div>
+          <a className="apps" id="appsid">
+            <div className="apps_top">
+              <span className="apps_title">Apps</span>
+              <span className="apps_playstore">availible on the playstore</span>
             </div>
-            <div className = 'apps_main'>
-              <div className = 'apps_main_one'>
+            <div className="apps_main">
+              <div className="apps_main_one">
                 <div>
-                  <img src = {vivahands} className='vivahands_img' alt = 'vivahands app'/>
+                  <img
+                    src={vivahands}
+                    className="vivahands_img"
+                    alt="vivahands app"
+                  />
                 </div>
-                <div className = 'vivahands_title_site'>
-                  <span className = 'vivahands_title'>Viva Hands</span>
-                  <span className = 'vivahands_site'>vivahands.com</span>
+                <div className="vivahands_title_site">
+                  <span className="vivahands_title">Viva Hands</span>
+                  <span className="vivahands_site">vivahands.com</span>
                 </div>
-                <div className = 'vivahands_info'>
-                  Vivahands is a live hand-holding application. Hold hands with your loved ones no
-                  matter the distance between.
+                <div className="vivahands_info">
+                  Vivahands is a live hand-holding application. Hold hands with
+                  your loved ones no matter the distance between.
                 </div>
               </div>
-              <div className = 'apps_main_two'>
+              <div className="apps_main_two">
                 <div>
-                  <img src = {logsnow} className='logsnow_img' alt = 'logsnow app'/>
+                  <img
+                    src={logsnow}
+                    className="logsnow_img"
+                    alt="logsnow app"
+                  />
                 </div>
-                <div className = 'logsnow_title_site'>
-                  <span className = 'logsnow_title'>Logs Now</span>
-                  <span className = 'logsnow_site'>logsnow.com</span>
+                <div className="logsnow_title_site">
+                  <span className="logsnow_title">Logs Now</span>
+                  <span className="logsnow_site">logsnow.com</span>
                 </div>
-                <div className = 'logsnow_info'>
-                  Logs Now is a utility designed to connect campers to local firewood suppliers
-                  and vice-versa.
+                <div className="logsnow_info">
+                  Logs Now is a utility designed to connect campers to local
+                  firewood suppliers and vice-versa.
                 </div>
               </div>
             </div>
           </a>
-          <img src = {tech} className='tech_img' alt = 'tech background'/>
-          <div className = 'skills'>
-            <div className = 'skills_right'>
-              <div className = 'skills_header_1'>
-                <span className = 'skills_title'>Skills</span>
-                <span className = 'skills_language'>markup/language/framework</span>
+          <img src={tech} className="tech_img" alt="tech background" />
+          <div className="skills">
+            <div className="skills_right">
+              <div className="skills_header_1">
+                <span className="skills_title">Skills</span>
+                <span className="skills_language">
+                  markup/language/framework
+                </span>
               </div>
-              <div className = 'skills_line'>
-                <img src = {line} className='line_img' alt = 'line'/>
+              <div className="skills_language_items">
+                <ul>
+                  <li>HTML5</li>
+                  <li>CSS3</li>
+                  <li>LESS</li>
+                  <li>SASS</li>
+                  <li>Javascript</li>
+                  <li>React</li>
+                  <li>Redux</li>
+                  <li>Node</li>
+                  <li>Express</li>
+                  <li>MongoDB</li>
+                  <li>Passport</li>
+                  <li>WebTokens</li>
+                  <li>Jest</li>
+                  <li>C</li>
+                </ul>
               </div>
-                <div className = 'skills_language_items'>
-                  <ul>
-                    <li>HTML5</li>
-                    <li>CSS3</li>
-                    <li>LESS</li>
-                    <li>SASS</li>
-                    <li>Javascript</li>
-                    <li>React</li>
-                    <li>Redux</li>
-                    <li>Node</li>
-                    <li>Express</li>
-                    <li>MongoDB</li>
-                    <li>Passport</li>
-                    <li>WebTokens</li>
-                    <li>Jest</li>
-                    <li>C</li>
-                  </ul>
-                </div>
-                <div className = 'skills_header_2'>
-                  <span>style/approach</span>
-                </div>
-                <div className = 'skills_line'>
-                  <img src = {line} className='line_img' alt = 'line'/>
-                </div>
-                <div className = 'skills_approach_items'>
-                  <ul>
-                    <li>CRUD</li>
-                    <li>REST</li>
-                    <li>Trello</li>
-                    <li>Figma</li>
-                  </ul>
-                </div>
-                <div className = 'skills_header_3'>
-                  <span>version control</span>
-                </div>
-                <div className = 'skills_line'>
-                  <img src = {line} className='line_img' alt = 'line'/>
-                </div>
-                <div className = 'skills_version_items'>
-                  <ul>
-                    <li>GIT</li>
-                    <li>Bash</li>
-                    <li>Command Line</li>
-                  </ul>
-                </div>
+              <div className="skills_header_2">
+                <span>style/approach</span>
+              </div>
+              <div className="skills_approach_items">
+                <ul>
+                  <li>CRUD</li>
+                  <li>REST</li>
+                  <li>Trello</li>
+                  <li>Figma</li>
+                </ul>
+              </div>
+              <div className="skills_header_3">
+                <span>version control</span>
+              </div>
+              <div className="skills_version_items">
+                <ul>
+                  <li>GIT</li>
+                  <li>Bash</li>
+                  <li>Command Line</li>
+                </ul>
+              </div>
             </div>
           </div>
-          <img src = {coffee} className='coffee_img' alt = 'coffee shop'/>
-              <ContactForm/>
-              <div className='thankyou'>
-                Typical response is within 48 hours.
-              </div>
-              <div className='fineprint'>
-              Designed and created by Nalee Riddell using Figma and VSCode. To see more of his work
-              please visit his Github.com page. Images for this site were acquired and are legal for
-              private and commercial use. Github and Linkedin are properties of their respective owners.
-              Vector logos for Github and Linkedin were acquired through SD:FLKJDSLK:FJSDL:KFJ
-              </div>
+          <img src={coffee} className="coffee_img" alt="coffee shop" />
+          <ContactForm />
+          <div className="fineprint">
+            Designed and created by Nalee Riddell using Figma and VSCode. To see
+            more of his work please visit his Github.com page. Images for this
+            site were acquired and are legal for private and commercial use.
+            Github and Linkedin are properties of their respective owners.
+            Vector logos for Github and Linkedin were acquired through
+            SD:FLKJDSLK:FJSDL:KFJ
           </div>
         </div>
-    )
+      </div>
+    );
   }
 }
-
